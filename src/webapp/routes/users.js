9dde67755940ffbea36a439d8a126fc5f1b0450c
Henry Huynh <huynh.vu2201@gmail.com>
2024-12-05 19:57:52 -0600

    adding user route

--- src/webapp/routes/users.js
+++ src/webapp/routes/users.js
@@ -21,3 +21,33 @@ const router = express.Router();
 
 //export default router;
 
+const express = require('express');
+const {PrismaClient} = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+
+// get all users
+router.get('/', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+// Get a user by ID
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { userId: Number(id) }
+    });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
