d205f2c086a162f9b19f1a9085bc0ecc179c2e72
Henry Huynh <huynh.vu2201@gmail.com>
2024-12-09 10:19:49 -0600

    update index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotenv = require('dotenv');
+const authRoutes = require('./src/routes/auth');
+const postRoutes = require('./src/routes/posts')
+const usersRoutes = require('./src/routes/users')
+
+// Load environment variables
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// use the routes
+
+app.use('/auth', authRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', usersRoutes);
+
+// Start the server
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+})
