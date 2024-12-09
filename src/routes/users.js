5e834f6036296d76918b8c883d794604a8a7255b
Henry Huynh <huynh.vu2201@gmail.com>
2024-12-08 21:42:10 -0600

    adding index.js for handling routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,26 +1,3 @@
-
-
-/*dummy 
-
-import express from "express";
-import {
-  getUser,
-  getUserFriends,
-  addRemoveFriend,
-} from "../controllers/users.js";
-import { verifyToken } from "../middleware/auth.js";
-
-const router = express.Router();
-
-/* READ */
-//router.get("/:id", verifyToken, getUser);
-//router.get("/:id/friends", verifyToken, getUserFriends);
-
-/* UPDATE */
-//router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
-
-//export default router;
-
 const express = require('express');
 const {PrismaClient} = require('@prisma/client');
 
