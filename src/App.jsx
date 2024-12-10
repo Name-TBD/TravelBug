8378e3fd5f01ba8c2cd09ae486cea6e2555bb727
Lucas Del Priore <lucasdelpriore@gmail.com>
2024-12-10 17:57:03 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react"
+
 import { Routes, Route } from "react-router-dom"
-import BookList from "./pages/Booklist"
-import BookDetails from "./pages/BookDetails"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Account from "./pages/Account"
-import NavBar from "./Navbar"
-import './index.css'
+import './App.css';
+
+import Home from './pages/Home';
+import NavBar from './pages/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Account from './pages/Account';
+
+
 
 const App = () => {
 
@@ -15,15 +17,15 @@ const App = () => {
       <NavBar />
       <h1>TravelBugs</h1>
       <Routes>
-        <Route path="/" element={<BookList />} />
-        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/account" element={<Account />} />
       </Routes>
        </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+    
     
