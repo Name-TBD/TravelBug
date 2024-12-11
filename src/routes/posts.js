57be565f59c68ec0cca8659711a25b5c7f555171
Henry Huynh <huynh.vu2201@gmail.com>
2024-12-11 12:24:04 -0600

    update posts.js

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import authenticateToken from '../middleware/authMiddleware';
 
+
 const prisma = new PrismaClient();
 const router = express.Router();
 
