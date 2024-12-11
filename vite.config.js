31853b9f48dffa911a162ad59b76e40523d5c29d
Henry Huynh <huynh.vu2201@gmail.com>
2024-12-11 11:22:48 -0600

    update again to connect to render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+// Load environment variables from .env file
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 4173
+  },
+  preview: {
+    port: process.env.PORT || 4173
+  }
+});
