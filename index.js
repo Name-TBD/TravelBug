7991a017de79eb8a7a90ea5343416089aa0beefc
Henry Huynh <huynh.vu2201@gmail.com>
2024-12-10 20:58:03 -0600

    refractor seed.js so it can be used in render

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-
-
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const authRoutes = require('./src/routes/auth');
-const postRoutes = require('./src/routes/posts')
-const usersRoutes = require('./src/routes/users')
-const uploadRoutes = require('.src/routes/upload'); // Added upload path for processing files during post creation
-
-
-
+const postRoutes = require('./src/routes/posts');
+const usersRoutes = require('./src/routes/users');
+const uploadRoutes = require('./src/routes/upload'); // Corrected the upload path
 
 // Load environment variables
-
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors({ origin: 'https://travelbugthugs.netlify.app' })); 
 app.use(express.json());
 
-// use the routes
-
+// Use the routes
 app.use('/auth', authRoutes);
 app.use('/post', postRoutes);
 app.use('/users', usersRoutes);
-app.use('/upload', uploadRoutes);   //Added upload path for processing files during post creation
+app.use('/upload', uploadRoutes); // Corrected the upload path
 
 // Start the server
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-})
+});
