08e49d8d0e513c077b8fff8cf0b1e7e87c054967
lucas <lucasdelpriore@gmail.com>
2024-12-13 08:16:54 -0600

    Installed MUI material icons and rebuilt dynamic Navbar with icons and state highlighting

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,40 @@
 
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import "../App.css";
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/account">Account</Link>
-        <Link to="/myprofile">MyProfile</Link>
-       
+      <div className="nav-left">
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+        >
+          <span className="material-icons">home</span>
+          Home
+        </NavLink>
+        <NavLink 
+          to="/account" 
+          className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+        >
+          <span className="material-icons">account_circle</span>
+          Account
+        </NavLink>
+        <NavLink 
+          to="/myprofile" 
+          className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+        >
+          <span className="material-icons">person</span>
+          My Profile
+        </NavLink>
+      </div>
+      <div className="nav-right">
+        <span className="material-icons nav-icon" tabIndex={0}>notifications</span>
+        <span className="material-icons nav-icon" tabIndex={0}>settings</span>
+        <span className="material-icons nav-profile">person</span>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
-
-/*
-
- <Link to="/postcatalogue">Posts</Link>
- 
- <Link to="/login">Login</Link>
-<Link to="/register">Register</Link>
-  <Link to="/feed">Feed</Link>
-  */
+export default Navbar;
